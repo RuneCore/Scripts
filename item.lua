8d378955c273aebbd037892d0e8ccde4072d7174
Sage <94207840+Sages0ft@users.noreply.github.com>
2022-02-16 14:21:45 +0100

    pass interface and slot

--- item.lua
+++ item.lua
@@ -3,7 +3,19 @@ local item = Item:new(123)
 item.some_cool_lua_variable = 69
 item.other_cool_lua_variable = "nice"
 
-function item:option1(player)
+function item:option1(player, interface, slot)
     print("called option1 on item!!111111")
     print(self.some_cool_lua_variable)
+    print(interface)
+    print(slot)
+
+    print("The interface is: " .. interface)
+
+    if slot == 5 then
+        print("slot is " .. slot)
+    end
+
+    if slot == 8 then
+        print("slot issss " .. slot)
+    end
 end
