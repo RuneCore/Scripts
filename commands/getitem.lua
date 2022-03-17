57ed2832031e9a1e3b5edfbbcbab52852f6a7da4
Sage <94207840+Sages0ft@users.noreply.github.com>
2022-03-17 17:44:01 +0100

    Added getitem command

---
+++ commands/getitem.lua
@@ -0,0 +1,23 @@
+local command = Command:new("getitem")
+
+function command:execute(player, args)
+    local item_slot = args[1]
+
+    -- validate if the first argument was passed
+    if item_slot == nil then
+        player:send_game_message("You must specify an item slot as first argument")
+        return
+    end
+
+    -- get the item from the inventory slot
+    local item = player:get_inventory_slot(INVENTORY.INVENTORY, item_slot)
+    if item == nil then
+        player:send_game_message("Item in slot " .. item_slot .. " was not found")
+        return
+    end
+
+    -- send the item's details to the player
+    player:send_game_message(
+        "Item in slot " .. item_slot .. " was found, its id is " .. item[1] .. " and its amount is " .. item[2]
+    )
+end
